3d89f2c429f9d4bfb3e4c21929a1359bf686cab3
anashamimi <anashamimi2@gmail.com>
2023-05-04 22:26:13 +0200

    add foreach

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
 <html lang="en">    
 
 <head>
@@ -9,8 +53,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
         integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script type="text/javascript" src="./js/script.js"></script>
@@ -21,10 +65,32 @@
 
 
 
-<body>    
-    <div id="app">
-
-    </div>    
+<body>  
+    
+    
+    <div class="container d-flex flex-column justify-content-center align-items-center">
+        <form action="">
+            <select name="" id="">
+                <option value="qualsiasi">Qualsiasi</option>
+                <option value="parcheggio">Con il parcheggio</option>
+                <option value="no-parcheggio">Senza parcheggio</option>
+            </select>
+        </form>
+        
+        <ul class="d-flex justify-content-center align-items-center">    
+            <?php foreach ($hotels as $hotel) { ?>
+                <li >
+                    <ul class="d-flex flex-column justify-content-center align-items-center card text-center">
+                        <li><span class="fw-bold">Nome:</span> <?php echo $hotel['name'] ?></li>
+                        <li><span class="fw-bold">Descrizione:</span> <?php echo $hotel['description'] ?></li>
+                        <li><span class="fw-bold">Voto:</span> <?php echo $hotel['vote'] ?></li>
+                        <li><span class="fw-bold">Distanza dal centro:</span> <?php echo $hotel['distance_to_center'] ?></li>
+                    </ul>                   
+                </li>
+            <?php } ?>
+       </ul>
+    </div>
+       
 </body>
 
 </html>
